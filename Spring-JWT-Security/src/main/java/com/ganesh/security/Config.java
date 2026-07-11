a3866bfa92aaf18eb2c0dd17311d7caab55c351e
Ganesh <ganeshgarje716@gmail.com>
2026-07-11 21:52:01 +0530

    Create Config Class for security and creating Security Methods for api handling

---
+++ Spring-JWT-Security/src/main/java/com/ganesh/security/Config.java
@@ -0,0 +1,38 @@
+package com.ganesh.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class Config {
+	
+	
+	
+	@Bean
+	public BCryptPasswordEncoder encoder() {
+		
+		return new BCryptPasswordEncoder();
+	}
+	
+	
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) {
+		
+		http.csrf(csrf->csrf.disable())
+		.authorizeHttpRequests(auth->auth.requestMatchers(".user/create")
+				.permitAll()
+				.anyRequest()
+				.authenticated())
+		.httpBasic(Customizer.withDefaults());
+		
+		return http.build();
+	}
+
+}
