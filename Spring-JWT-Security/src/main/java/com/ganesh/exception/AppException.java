ae665578e0e3026319e6a516d93bfc19b72572d5
Ganesh <ganeshgarje716@gmail.com>
2026-07-11 23:47:58 +0530

    Create AppException Class For Handling Exceptions

---
+++ Spring-JWT-Security/src/main/java/com/ganesh/exception/AppException.java
@@ -0,0 +1,19 @@
+package com.ganesh.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class AppException {
+	
+	
+	
+	@ExceptionHandler(value = Exception.class)
+	public ResponseEntity<String> handalException(Exception e) {
+		
+		return new ResponseEntity<String>(e.getMessage(),HttpStatus.UNAUTHORIZED);
+	}
+
+}
