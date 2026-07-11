75e95b1c226bc0987937032c27601966db530b75
Ganesh <ganeshgarje716@gmail.com>
2026-07-11 23:40:11 +0530

    .

--- Spring-JWT-Security/src/main/java/com/ganesh/security/Config.java
+++ Spring-JWT-Security/src/main/java/com/ganesh/security/Config.java
@@ -3,8 +3,10 @@ package com.ganesh.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -34,7 +36,7 @@ public class Config {
 	public SecurityFilterChain filterChain(HttpSecurity http) {
 		
 		http.csrf(csrf->csrf.disable())
-		.authorizeHttpRequests(auth->auth.requestMatchers("/user/create")
+		.authorizeHttpRequests(auth->auth.requestMatchers("/user/create", "/authenticate")
 				.permitAll()
 				.anyRequest()
 				.authenticated())
@@ -53,6 +55,14 @@ public class Config {
 		provider.setPasswordEncoder(encoder());
 		
 		return provider;
+	}
+	
+	
+	
+	@Bean
+	public AuthenticationManager authManager(AuthenticationConfiguration auth)
+	{
+		return auth.getAuthenticationManager();
 	}
 
 }
